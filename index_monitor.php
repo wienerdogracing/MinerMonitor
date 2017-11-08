b7d8e115f24f783e8da5afe0cdb26abf185c031f
Derrick Emery <derrick.emery@tdxp.net>
2017-11-08 00:35:44 -0600

    Moved hostfile to config.ini getsock now uses stream_socket Outputs actual error now instead of default "No connection" Cleaned up code and added comments

--- index_monitor.php
+++ index_monitor.php
@@ -1,7 +1,6 @@
 <?php
-/* cpuminer API sample UI */
+require_once 'getConfig.php';
 
-$hostsFile = '/var/www/public_html/minerHosts'; // modify this path to the minerHosts file
 $totalHashRate = 0;
 $json = '';
 
@@ -12,36 +11,39 @@ error_reporting(0);
 defined('API_HOST') || define('API_HOST', 'localhost');
 defined('API_PORT') || define('API_PORT', 4048);
 
-/*
-Modified getsock to use fsock for better compatability. 
-It's a blocking method, but it should still run pretty fast, even with many machines to check.
-TODO: Will add multi-threads later.
-*/
-
 function getsock($host,$port,$cmd) {
-	// Seconds to wait for a successful connection
-    $connectTimeout = 5;
-    // Seconds to wait to receive data
-    $receiveTimeout = 5;
 
-    $socket = @fsockopen($host, $port, $errNo, $errStr, $connectTimeout);
+    $timeout = 5;
+    $socket = @stream_socket_client(
+            "tcp://$host:$port",
+            $errNo,
+            $errorMessage,
+            $timeout,
+        STREAM_CLIENT_CONNECT,
+            stream_context_create(array())
+    );
 
-    if( !$socket || !stream_set_timeout( $socket, $receiveTimeout ) )
-    {
-        return FALSE;
+    if ($socket === false) {
+        //throw new UnexpectedValueException("Failed to connect: $errorMessage");
+        $data = "error=Failed to connect: $errorMessage|";
     }
-
-    fwrite( $socket, $cmd );
-    $data = fread( $socket, 256 );
-    fclose( $socket );
-	$data = strToArray($data);
+    else {
+        fwrite($socket, $cmd);
+        stream_set_timeout($socket, 2);
+        $data = stream_get_contents($socket);
+        $info = stream_get_meta_data($socket);
+        fclose($socket);
+        if ($info['timed_out']) {
+            $data = 'error=Connection timed out|';
+        }
+    }
+    $data = strToArray($data);
     return $data;
 }
 
 
 function strToArray($sockData) {
 
-	$objs = array();
 	$objs = explode('|', $sockData);
 	array_pop($objs);
 	$data = array();
@@ -57,9 +59,9 @@ function strToArray($sockData) {
 }
 
 
-function getDataFromHost($host)
+function getDataFromHost($host,$port)
 {
-	$summary=getsock($host,API_PORT,'summary');
+	$summary=getsock($host,$port,'summary');
 	if ($summary==false) return false;
     return json_encode(array('summary'=>$summary));
 }
@@ -68,12 +70,11 @@ function getDataFromHost($host)
 function getdataFromPeers()
 {
 	global $configs, $hostsFile;
-
     $configs= file($hostsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
 	$data = array();
-	foreach ($configs as $name => $conf) {
-		$data[$conf] = json_decode(getDataFromHost($conf), TRUE);
+	foreach ($configs as $host) {
+	    $conf = explode(':',$host);
+		$data[$host] = json_decode(getDataFromHost($conf[0],(isset($conf[1])) ? $conf[1] : API_PORT), TRUE);
 	}
 	return $data;
 }
@@ -104,7 +105,7 @@ function translateField($key)
 	$intl['UPTIME'] = 'Miner up time';
 	$intl['TS'] = 'Last update';
 
-	$intl['TEMP'] = 'T°c';
+	$intl['TEMP'] = 'T c';
 	$intl['FAN'] = 'Fan %';
 	$intl['FREQ'] = 'CPU Freq.';
 	$intl['PST'] = 'P-State';
@@ -129,7 +130,7 @@ function translateValue($key,$val,$data=array())
 			}
 			break;
 		case 'NAME':
-			$val = $data['NAME'].'&nbsp;'.$data['VER'];
+			$val = $data['NAME'].'ï¿½&nbsp;'.$data['VER'];
 			break;
 		case 'FREQ':
                         $val = sprintf("%.2fGHz", floatval($val)/1000000);
@@ -144,44 +145,32 @@ function translateValue($key,$val,$data=array())
 function displayData($data)
 {
 	global $totalHashRate;
-    $status = 'No connection';
-	$noconnection = '';
     $htm = '';
-    $htm1 = '';
-	$totals = array();
+    $totals = '';
 	$totalHashRate = 0;
-
 	foreach ($data as $name => $stats) {
-
 		$htm .= '<table id="tb_'.$name.'" class="stats">'."\n";
 		$htm .= '<tr><th class="machine" colspan="2">'.$name."</th></tr>\n";
-		if ($stats == NULL) { //issue with connection or miner did not return any data
-			   $htm .= '<tr><td class="key">'.$status.'</td>'.
-               '<td class="val">'.$noconnection."</td></tr>\n";
-        }
-        else {
-        	$summary = (array) $stats['summary'];    
-            foreach ($summary as $key=>$val) {
-                    if (!ignoreField($key))
-					$htm .= '<tr><td class="key">'.translateField($key).'</td>'.
-						'<td class="val">'.translateValue($key, $val, $summary)."</td></tr>\n";
-                    if ($key=='KHS') {
-                       $totalHashRate += $val;
+        $summary = (array) $stats['summary'];
+        foreach ($summary as $key=>$val) {
+            if (!ignoreField($key)) {
+                if ($key == 'error') {
+                    $htm .= '<tr><td class="val" colspan="2">' . translateValue($key, $val, $summary) . "</td></tr>\n";
+                } else {
+                    $htm .= '<tr><td class="key">' . translateField($key) . '</td>' .
+                        '<td class="val">' . translateValue($key, $val, $summary) . "</td></tr>\n";
+                    if ($key == 'KHS') {
+                        $totalHashRate += $val;
                     }
+                }
             }
-		}
-		$htm .= "</table>\n";
+        }
+        $htm .= "</table>\n";
 	}
 	// totals
 
-        $htm1 .= '<div class="totals"><h2>Totals:<br />KH/S: '.$totalHashRate.'<br />KH/M: '.($totalHashRate*60).'</h2>'."\n";
-	    //$htm .= '<li><span class="algo">'.$algo.":</span>$hashrate kH/s</li>\n";
-        $htm1 .= '</div>';
-        //$htm2 = $htm1.$htm;
-        $htm = $htm1.$htm;
-	
-	//echo "Total Hash Rate: $totalHashRate";
-	return $htm;
+    $totals = '<div class="totals"><h2>Totals:<br />KH/S: '.$totalHashRate.'<br />KH/M: '.($totalHashRate*60).'</h2></div>'."\n";
+	return $totals.$htm;
 }
 
 function displayHashTotal()
@@ -197,11 +186,4 @@ $data = getdataFromPeers();
 $totalHash = displayHashTotal();
 
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
 <?=displayData($data)?>
-</body>
-</html>
